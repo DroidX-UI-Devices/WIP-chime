bfb11415645dc6bf1e252adc4f5e5787a3aa54b1
Notganesh <chimeports@gmail.com>
2023-09-27 18:47:58 +0530

    chime:updated vendorsetup with miui 14
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-#Rom Source Patches
+# ROM source patches
 
 color="\033[0;32m"
 end="\033[0m"
@@ -8,12 +8,19 @@ sleep 1
 
 
 # Clone kernel
-git clone https://github.com/Notganesh/kernel_xiaomi_chime.git -b main kernel/xiaomi/chime
+git clone https://github.com/hac4us06/kernel-xiaomi-electro -b main kernel/xiaomi/chime
+# Setup kernel
+(cd kernel/xiaomi/chime; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
+
+rm -rf system/libhidl
+then (not imp. but anyways)
+git clone https://github.com/ArrowOS/android_system_libhidl.git -b arrow-13.0 system/libhidl
 
 # Clone vendor
-git clone https://github.com/Notganesh/vendor_xiaomi_chime.git -b thirteen vendor/xiaomi/chime
-git clone https://github.com/Notganesh/vendor_xiaomi_citrus.git -b thirteen vendor/xiaomi/citrus
-git clone https://gitlab.com/chimeoss/vendor_xiaomi_lime.git -b lineage-20 vendor/xiaomi/lime
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_chime -b thirteen vendor/xiaomi/chime
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_chime-perf -b thirteen  vendor/xiaomi/chime-perf
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_citrus.git -b thirteen vendor/xiaomi/citrus
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_lime.git -b thirteen vendor/xiaomi/lime
 
 # Hardware
-git clone https://github.com/Notganesh/hardware_xiaomi.git -b Tiramisu hardware/xiaomi 
+git clone https://github.com/hac4us06/hardware_xiaomi -b thirteen hardware/xiaomi
